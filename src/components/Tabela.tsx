6117958d2f93503c07a2f9eb09dced3a5cb0567c
dadyilhawallacevidal <wallace.vidal@dadyilha.com.br>
2023-11-17 16:59:21 -0300

    TAbela

---
+++ src/components/Tabela.tsx
@@ -0,0 +1,17 @@
+import Cliente from "@/core/Cliente";
+import { useEffect } from "react";
+
+interface TabelaProps {
+    clientes: Cliente[];
+    
+}
+
+export default function Tabela(props : TabelaProps){  
+    return(
+        <table>
+            
+        </table>
+    );
+}
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Layout from './components/Layout';
+import Layout from '../components/Layout';
+import Tabela from '../components/Tabela';
+import Cliente from '@/core/Cliente';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Carlos", 23, "3"),
+    new Cliente("Pedro", 54, "4"),
+  ];
   return (
     <div className={`
         flex h-screen justify-center items-center
         bg-gradient-to-r from-blue-500   to-purple-500
         `}>
       <Layout titulo='Cadastro Simples'>
-        <span>
-          Conteudo
-        </span>
+        <Tabela clientes={clientes}/>
       </Layout>
     </div>
   );
