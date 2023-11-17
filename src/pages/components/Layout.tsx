78768cfdb47d3984213155be0a134f59e8bb49c2
dadyilhawallacevidal <wallace.vidal@dadyilha.com.br>
2023-11-17 15:28:17 -0300

    First Commit

---
+++ src/pages/components/Layout.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Titulo from "./Titulo";
+
+interface LayoutProps {
+    titulo: string;
+    children: any
+}
+
+export default function Layout(props: LayoutProps){
+    var children : any;
+    if(props.children.legnth){
+        children = props.children.map(
+            (child: any, i : any) => React.cloneElement((child), {key: i})
+        );
+    }
+    else{
+        children = React.cloneElement((props.children));
+    }
+    return(
+        <div className={`
+            flex flex-col w-2/3
+            bg-white text-gray-800
+            rounded-md
+        `}>
+            <Titulo>
+                {props.titulo}
+            </Titulo>
+            <div className="p-6">
+                {
+                    children
+                }
+            </div>
+        </div>
+    );
+}
